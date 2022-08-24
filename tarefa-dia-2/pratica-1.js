01f41d55c677ae9d1719b95751b29732fe2c0735
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2022-08-24 20:20:21 -0300

    4.0
    Commit exercicio da aula 01!

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,14 @@
+let nome = "Talilo Tarlison";
+let email = "ttarlison@mail.com";
+let profissao = "Estudante";
+let idade = 27
+let localDosSonhos = "New York City";
+
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(localDosSonhos);
+
+
+
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${localDosSonhos}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
